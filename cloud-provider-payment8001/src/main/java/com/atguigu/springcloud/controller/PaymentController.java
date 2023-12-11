799d04ff5ee36274989250c9d8da9d2ceb6907fb
Coco <Coco920177720115@outlook.com>
2023-12-11 21:55:48 +0800

    代码测试

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -17,7 +17,7 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
-    @PostMapping(value = "/payment/create ")
+    @PostMapping(value = "/payment/create")
     public CommonResult create(Payment payment) {
         int result = paymentService.create(payment);
         log.info("*****插入结果" + result);
@@ -28,7 +28,7 @@ public class PaymentController {
         }
     }
 
-    @GetMapping(value = "/payment/get/{id} ")
+    @GetMapping(value = "/payment/get/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
         log.info("*****查询结果" + payment);
